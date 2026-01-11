737c732220fc5ab32c525e11f9346448195838d7
hiep2004 <buituanhiepvinhtuong@gmail.com>
2026-01-12 01:08:50 +0700

    Init Moji project

---
+++ chat-realtime-backend/src/main/java/com/tuhu/chat_realtime_backend/Application.java
@@ -0,0 +1,13 @@
+package com.tuhu.chat_realtime_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ chat-realtime-backend/src/test/java/com/tuhu/chat_realtime_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuhu.chat_realtime_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
